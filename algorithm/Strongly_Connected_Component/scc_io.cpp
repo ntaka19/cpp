c193fae1d1efc7d342d07aea01b0859a3410d94e
ntaka19 <naruakitakano@gmail.com>
2017-07-26 00:48:21 +0900

    new files ID edgelist

--- algorithm/Strongly_Connected_Component/scc_io.cpp
+++ algorithm/Strongly_Connected_Component/scc_io.cpp
@@ -1,23 +1,17 @@
-//強い連結成分分解(Strongly Connected Component
-//二度のdfsで実装
-//強連結成分を一つの点に潰すと、DAG(閉路を持たない有向グラフ)
-
-//関係ないメモ：
-/*グローバル変数に頼りすぎると、「機能単位の独立したモジュール化」という関数本来の目的が失われることになるわけです。
-従って、基本的には「できる限りローカル変数を用いて、関数とは引数と返却値でやり取りする」という習慣をつけた方がよいでしょう。
-*/
-/////////
+//ファイル読み込み可能にする
+//
+//
 #include<iostream>
 #include<cstdlib>
 #include<cstdio>
 #include<vector>
 #include<cstring>
+#include<fstream>
 
-
-#define MAX_V 4 
+#define MAX_V 10000 
 
 using namespace std;
-int V = 4;
+int V;
 
 vector<int> G[MAX_V]; //グラフの隣接リストを表現
 vector<int> rG[MAX_V]; //辺の向きを逆にしたグラフ
@@ -30,19 +24,34 @@ void rdfs(int v, int k);
 int scc();
 
 int main(){
-  add_edge(0,1);
-  add_edge(1,0);
-  add_edge(1,2);
-  add_edge(2,3);
-  add_edge(3,1);
-  //実際のデータを入れる場合、ID?を標準入力させる？
-  //IDの入力の場合どうするか？
-
-  printf("scc = %d\n",scc());  
   
-  //int j = vs.size();
-  //printf("vs size %d\n",j);
+  //cout << "ifstream" << endl;
+  ifstream ifs("data.txt");
+  string str;
 
+  if(ifs.fail()){
+    cerr<<"File do not exist\n";
+    exit(0);
+  }
+
+  int a,b; //max is the largest node number
+  int max = 0;
+  while(getline(ifs,str)){
+    sscanf(str.data(), "%d %d", &a, &b);
+    add_edge(a,b);
+    
+    if(a>b && a>max){
+      max = a;
+    }else if(b>=a && b>max){
+     max = b;
+    }
+
+    cout << a << " " << b << endl;
+  }
+
+  V = max+1;
+  printf("largest node number = %d\n",max);
+  printf("scc = %d\n",scc());  
 }
 
 //辺の向きを順方向と逆方向で入れる。
@@ -55,6 +64,7 @@ void dfs(int v){
   used[v] = true;
   for(int i = 0;i < G[v].size(); i++){
     if(!used[G[v][i]]) dfs(G[v][i]);
+
   }
   vs.push_back(v);
 }
